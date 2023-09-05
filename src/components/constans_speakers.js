e2b11545eaac4297f2b4733ccc1e7b21ba2eb754
JoseZR <josezerrod@hotmail.com>
2023-09-05 13:45:49 -0600

    se agrega imagenes de ponentes

--- src/components/constans_speakers.js
+++ src/components/constans_speakers.js
@@ -5,7 +5,7 @@ const speakers = [
     name: 'Oliver Castellanos',
     company: 'Schneider Electric',
     rol: 'Solution Architect',
-    avatar: '/futuristics-minds/speakers/oliver-castellanos.webp',
+    avatar: '/futuristic-minds/speakers/oliver-castellanos.webp',
     sketch: 'Ingeniero Mecatrónico egresado de la Universidad Marista. Ha trabajado en diseño, ejecución y supervisión de proyectos de aire acondicionado y especiales con Hubard y Bourlon, ahora CICSA de Grupo Carso. Actualmente, se desempeña como Solution Architect de Building Management System Control de acceso y Medición Eléctrica, apoyando a la validación en la prescripción de productos en varios proyectos. También se encarga de impartir las certificaciones en los softwares de las soluciones, previamente mencionados. Fue miembro asociado de ASHRAE Capítulo Ciudad de México.'
   },
   {
@@ -14,7 +14,7 @@ const speakers = [
     name: 'Sergio Romero',
     company: 'Schneider Electric',
     rol: 'Business Developer Manager for Power Quality & Energy Automation',
-    avatar: '/futuristics-minds/speakers/sergio-romero.webp',
+    avatar: '/futuristic-minds/speakers/sergio-romero.webp',
     sketch: 'Ingeniero eléctrico egresado de la UNAM. Especialista en medición eléctrica, calidad de la energía, sistemas de monitoreo (EMS). Apasionado de la tecnología, la innovación y las soluciones inteligentes. Experto en  soluciones que involucran el monitoreo de equipos en media y baja tensión para concentrar lecturas en software para la generación de reportes, monitoreo en tiempo real y registro de históricos.'
   },
   {
@@ -23,7 +23,7 @@ const speakers = [
     name: 'Mariana Cruz',
     company: 'Schneider Electric',
     rol: 'Sales Distributors Engineer',
-    avatar: '/futuristics-minds/speakers/mariana-cruz.webp',
+    avatar: '/futuristic-minds/speakers/mariana-cruz.webp',
     sketch: 'Egresada del Instituto Politécnico Nacional, en la carrera de Ingeniería en Control y Automatización con un posgrado en Administración de Negocios enfocado en Mercadotecnia, en la Universidad del Valle de México.  Profesional con 10 años de experiencia en el ámbito Industrial.  Actualmente se desempeña como Sales Distribuitors Engineer en Schneider Electric Mexico, enfocada principalmente en brindar soluciones de control y automatización a usuarios finales, Integradores y OEMs.'
   },
   {
@@ -32,7 +32,7 @@ const speakers = [
     name: 'Leonardo Pérez',
     company: 'Endress + Hauser Mexico',
     rol: 'Industry Manager',
-    avatar: '/futuristics-minds/speakers/img-man.webp',
+    avatar: '/futuristic-minds/speakers/img-man.webp',
     sketch: 'Ingeniero en Comunicaciones y Electrónica en la especialidad de Control Desarrollando conocimiento y experiencia en temas relacionados con: Protocolos de Comunicación: Foundation Fieldbus, Profibus, Modbus, HART, WirelessHART y OPC. Plataforma Rockwell: Controladores y Módulos, RSLogix5000, RSEnergyMetrix, RSFieldbus Plataforma E+H: Instrumentos y componentes, Tankvision, SupplyCare, eSight, FieldCare Clasificación y métodos de protección para áreas clasificadas.'
   },
   {
@@ -41,7 +41,7 @@ const speakers = [
     name: 'Sac Nité Arzate',
     company: 'Schneider Electric',
     rol: 'Business Developer',
-    avatar: '/futuristics-minds/speakers/sac-nite.webp',
+    avatar: '/futuristic-minds/speakers/sac-nite.webp',
     sketch: 'Líder de pronóstico experimentado y analista de mercado con más de 10 años de experiencia en diversas industrias. Perfil analítico con importante enfoque en modelos matemáticos, análisis de estacionalidad, seguimiento de proyectos, comunicación y networking con plantas, marketing y áreas comerciales. Experiencia en marketing de inteligencia de negocios como asesor de confianza para áreas comerciales, medianas y pequeñas organizaciones con comprensión de casos de uso de negocios verticales y funcionales en México. Buena comprensión de los revendedores, OEM, integración de sistemas y otros socios "influyentes". Fuerte liderazgo y habilidades de colaboración entre equipos. Historial demostrable de "liderazgo de pensamiento" original y apasionado dentro de Marketing. Historial de creación exitosa de alineación con ventas e influencia en una organización matricial.'
   },
   {
@@ -50,7 +50,7 @@ const speakers = [
     name: 'Alfonso Feria Kaiser',
     company: 'Jet Propulsion Laboratory',
     rol: 'Ingeniero de Sistemas',
-    avatar: '/futuristics-minds/speakers/alfonso-feria.webp',
+    avatar: '/futuristic-minds/speakers/alfonso-feria.webp',
     sketch: 'El doctor Alfonso Feria Kaiser obtuvo su licenciatura y maestría en Ingeniería Mecánica de la UNAM y un doctorado en la Universidad de Wisconsin. Ha trabajado en JPL desde 1992, en donde comenzó haciendo diseño y análisis estructural de antenas de comunicaciones, y diseño y desarrollo de estructuras inflables. Fue supervisor del Grupo de Ingeniería Mecánica y Estructural de Antenas, líder del equipo de análisis estructural de la misión de interferometría espacial y analista de la fuente de rayos X de CheMin, un instrumento en el Rover Opportunity. Alfonso trabajó como jefe de ingeniería del equipo mecánico para el ensamblado de naves espaciales y fue ingeniero del MIRI Crycooler, que es parte del JWST. Últimamente, trabajó en el Complejo de Comunicaciones del Espacio Profundo de Madrid durante la instalación de dos nuevas estaciones de seguimiento. Actualmente, es Ingeniero de sistemas de la Red del Espacio Profundo.'
   },
 ]
