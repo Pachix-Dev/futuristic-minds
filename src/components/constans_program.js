b9e2f161b26364c4a2890f5c559af3adb3546deb
JoseZR <josezerrod@hotmail.com>
2023-09-25 18:36:49 -0600

    se agrega foto de ponente

--- src/components/constans_program.js
+++ src/components/constans_program.js
@@ -667,7 +667,7 @@ const programITM = {
         resume: 'Daremos a conocer la importancia de las habilidades sociales del siglo XXI. Mediante ejercicios de reflexión y análisis, buscamos hacer más conscientes a los jóvenes sobre que estas competencias son cada vez más importantes en el entorno laboral y social actual, para tener éxito en un mundo caracterizado por la interconexión global, la tecnología y el mundo digital. '
       },
       {
-        id: 0,
+        id: 28,
         id_s2: 0,
         hour: '11:00 hrs',
         duration: '45 min',
