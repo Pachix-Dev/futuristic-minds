b98459e408734b80d99873dbaf319998680358e5
JoseZR <josezerrod@hotmail.com>
2023-09-28 16:24:08 -0600

    cambio en titulo de descripcion

--- src/components/constans_speakers.js
+++ src/components/constans_speakers.js
@@ -73,7 +73,7 @@ const speakers = [
   },
   {
     id: 9,
-    conference: 'Diseño especulativo para resolver problemas',
+    conference: 'Economía circular ¿Una alternativa para resolver las problemáticas ambientales?',
     name: 'Ramón Ayala Álvarez',
     company: 'Escuela de Talentos Guanajuato Azteca',
     rol: 'Profesor de Innovación y Emprendimiento',
